3d2bc1bfe64abc8b9d67ac16cb3783c245dcd8eb
azizshoxofficial <azizshoxofficial@gmail.com>
2022-10-06 13:51:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+console.log("Js is started");
+
+
+// Compare operators
+
+// console.log( 5 > 3);
+// console.log( 5 < 3);
+// console.log( 7 == 7);
+// console.log( 12 == 25);
+// console.log( 3 != 6);
+
+
+// == - faqat qiymatni tekshiradi
+
+// === - oldin type keyin qiymatini tekshiradi
+
+// console.log( 5 === 5 );
+
+// console.log( "salom" === "salom");
+
+
+// let result = 5 === 6
+
+// console.log("result>>>", result);
+
+// if( result){
+//     console.log("ha");
+// }else{
+//     console.log("Yo'q");
+// }
+
+
+// % - qoldiqni topadi
+
+// console.log( 10 % 5); // 0
+// console.log( 10 % 3); // 0
+// console.log( 25 % 35); // 0
+// console.log( 10 % 98); // 0
+
+
+// let son = 13
+
+// if( son % 2 === 0 ) {
+//     console.log("Juft son");
+// }else{
+//     console.log("Toq son");
+// }
+
+
+// let yosh = 18
+
+// if( yosh >= 18){
+//     console.log("katta yosh");
+// }else{
+//     console.log("kichik bola");
+// }
+
+
+let pul = 50
+
+if(pul >= 500){
+    console.log("iPhone 11 pro max");
+}else if(pul >=300){
+    console.log("Galaxy S 20");
+}else if(pul >=100){
+    console.log( "Redmi");
+}else {
+    console.log("Nokia ishlataver gadoy");
+}
